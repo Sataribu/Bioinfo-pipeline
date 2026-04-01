cc33cb19f07d9a2f5f4df1152d2dabf50cf954e7
Ataribu Ovuzonye Samuel <ataribuovuzonyesamuel@gmail.com>
2026-04-01 11:10:38 +0200

    Intitial commit

--- src/pipeline.R
+++ src/pipeline.R
@@ -1,31 +1,8 @@
-author:samuel
-#!/bin/bash
-#PBS -N quality test
-#PBS -l walltime=1:00:00
-#PBS -l ncpus=16
-#PBS -l mem=32GB
-#PBS -M 31030815@sun.ac.za
-#PBS -m abe
-
-cd $PBS_O_WORKDIR
-
-module load app/sar
-
-print("Quality check 1: Input received")
-print("Quality check 2: Validating data")
-print("Quality check 3: Checking format")
-print("Quality check 4: Removing duplicates")
-print("Quality check 5: Verifying values")
-print("Quality check 6: Checking ranges")
-print("Quality check 7: Cleaning data")
-print("Quality check 8: Logging results")
-print("Quality check 9: Final validation")
-print("Quality check 10: Process complete")
-
-if validation<0{
-print{"hypothesis error}
-
-else validation>0{
-print{reject H0}
-
-}
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABDH3E3cgn
+lstd0g9PjGQPQkAAAAGAAAAAEAAAAzAAAAC3NzaC1lZDI1NTE5AAAAINaSCT6g+OU8seht
+hG67+3UM3W+llj7JK/AbBQWSQqgxAAAAsJyr7N1eYygbERJ2biD92zQG+MPV7ytIdSbFPZ
+3jGqn+uVZbMlwYx3x2qLC1mIkFuNw6imcH0DZyXO/q7IbxR4OcYfQJDip/OANcAth0T87K
+fH9/va4q6mdCqlnIi1Q7VdHxo67jWkIxwYNNwRQJFSHdtOte7nR201KaTtZT3PK4QwSE56
+WeJD2/gGBBaZuHksI7jsi3/KKHgbeormUcGcDX0No6VA80WTMoAurUAhie
+-----END OPENSSH PRIVATE KEY-----
